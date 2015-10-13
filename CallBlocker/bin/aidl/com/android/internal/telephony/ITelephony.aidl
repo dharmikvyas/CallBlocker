81494122a723283e706d4ef42102bef6c755d444
MyMac <dharmik@wp-coders.com>
2015-10-13 13:23:10 +0530

    Call Blocker test app

---
+++ CallBlocker/bin/aidl/com/android/internal/telephony/ITelephony.aidl
@@ -0,0 +1,10 @@
+package com.android.internal.telephony;
+
+// Need for Android Telephony
+// Please keep the package, class and method names as it is.
+
+  interface ITelephony {
+
+    boolean endCall();
+
+  }
---
+++ CallBlocker/gen/com/android/internal/telephony/ITelephony.java
@@ -0,0 +1,95 @@
+/*
+ * This file is auto-generated.  DO NOT MODIFY.
+ * Original file: /Volumes/New Volume/Songs/hardik/CallBlocker/src/com/android/internal/telephony/ITelephony.aidl
+ */
+package com.android.internal.telephony;
+// Need for Android Telephony
+// Please keep the package, class and method names as it is.
+
+public interface ITelephony extends android.os.IInterface
+{
+/** Local-side IPC implementation stub class. */
+public static abstract class Stub extends android.os.Binder implements com.android.internal.telephony.ITelephony
+{
+private static final java.lang.String DESCRIPTOR = "com.android.internal.telephony.ITelephony";
+/** Construct the stub at attach it to the interface. */
+public Stub()
+{
+this.attachInterface(this, DESCRIPTOR);
+}
+/**
+ * Cast an IBinder object into an com.android.internal.telephony.ITelephony interface,
+ * generating a proxy if needed.
+ */
+public static com.android.internal.telephony.ITelephony asInterface(android.os.IBinder obj)
+{
+if ((obj==null)) {
+return null;
+}
+android.os.IInterface iin = obj.queryLocalInterface(DESCRIPTOR);
+if (((iin!=null)&&(iin instanceof com.android.internal.telephony.ITelephony))) {
+return ((com.android.internal.telephony.ITelephony)iin);
+}
+return new com.android.internal.telephony.ITelephony.Stub.Proxy(obj);
+}
+@Override public android.os.IBinder asBinder()
+{
+return this;
+}
+@Override public boolean onTransact(int code, android.os.Parcel data, android.os.Parcel reply, int flags) throws android.os.RemoteException
+{
+switch (code)
+{
+case INTERFACE_TRANSACTION:
+{
+reply.writeString(DESCRIPTOR);
+return true;
+}
+case TRANSACTION_endCall:
+{
+data.enforceInterface(DESCRIPTOR);
+boolean _result = this.endCall();
+reply.writeNoException();
+reply.writeInt(((_result)?(1):(0)));
+return true;
+}
+}
+return super.onTransact(code, data, reply, flags);
+}
+private static class Proxy implements com.android.internal.telephony.ITelephony
+{
+private android.os.IBinder mRemote;
+Proxy(android.os.IBinder remote)
+{
+mRemote = remote;
+}
+@Override public android.os.IBinder asBinder()
+{
+return mRemote;
+}
+public java.lang.String getInterfaceDescriptor()
+{
+return DESCRIPTOR;
+}
+@Override public boolean endCall() throws android.os.RemoteException
+{
+android.os.Parcel _data = android.os.Parcel.obtain();
+android.os.Parcel _reply = android.os.Parcel.obtain();
+boolean _result;
+try {
+_data.writeInterfaceToken(DESCRIPTOR);
+mRemote.transact(Stub.TRANSACTION_endCall, _data, _reply, 0);
+_reply.readException();
+_result = (0!=_reply.readInt());
+}
+finally {
+_reply.recycle();
+_data.recycle();
+}
+return _result;
+}
+}
+static final int TRANSACTION_endCall = (android.os.IBinder.FIRST_CALL_TRANSACTION + 0);
+}
+public boolean endCall() throws android.os.RemoteException;
+}
